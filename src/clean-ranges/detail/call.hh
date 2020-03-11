6ffdfd5664e0f52282c5db644985eaf1fc173b87
Philip Trettner <Philip.Trettner@rwth-aachen.de>
2020-03-11 10:47:19 +0100

    fixed ambiguous call

--- src/clean-ranges/detail/call.hh
+++ src/clean-ranges/detail/call.hh
@@ -3,27 +3,28 @@
 #include <type_traits>
 
 #include <clean-core/forward.hh>
+#include <clean-core/priority_tag.hh>
 
 namespace cr::detail
 {
 template <class F, class Arg>
-constexpr auto impl_obj_member_call(F&& fun, Arg&& arg) -> decltype(arg.*fun)
+constexpr auto impl_obj_member_call(F&& fun, Arg&& arg, cc::priority_tag<1>) -> decltype(arg.*fun)
 {
     return arg.*fun;
 }
 template <class F, class Arg>
-constexpr auto impl_obj_member_call(F&& fun, Arg&& arg) -> decltype((*arg).*fun)
+constexpr auto impl_obj_member_call(F&& fun, Arg&& arg, cc::priority_tag<0>) -> decltype((*arg).*fun)
 {
     return (*arg).*fun;
 }
 
 template <class F, class Arg>
-constexpr auto impl_fun_member_call(F&& fun, Arg&& arg) -> decltype((arg.*fun)())
+constexpr auto impl_fun_member_call(F&& fun, Arg&& arg, cc::priority_tag<1>) -> decltype((arg.*fun)())
 {
     return (arg.*fun)();
 }
 template <class F, class Arg>
-constexpr auto impl_fun_member_call(F&& fun, Arg&& arg) -> decltype(((*arg).*fun)())
+constexpr auto impl_fun_member_call(F&& fun, Arg&& arg, cc::priority_tag<0>) -> decltype(((*arg).*fun)())
 {
     return ((*arg).*fun)();
 }
@@ -32,9 +33,9 @@ template <class F, class Arg>
 constexpr decltype(auto) call(F&& fun, Arg&& arg)
 {
     if constexpr (std::is_member_object_pointer_v<std::remove_reference_t<F>>)
-        return impl_obj_member_call(cc::forward<F>(fun), cc::forward<Arg>(arg));
+        return impl_obj_member_call(cc::forward<F>(fun), cc::forward<Arg>(arg), cc::priority_tag<1>{});
     else if constexpr (std::is_member_function_pointer_v<std::remove_reference_t<F>>)
-        return impl_fun_member_call(cc::forward<F>(fun), cc::forward<Arg>(arg));
+        return impl_fun_member_call(cc::forward<F>(fun), cc::forward<Arg>(arg), cc::priority_tag<1>{});
     else
         return fun(cc::forward<Arg>(arg));
 }
